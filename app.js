baf265ab014743deb06777685b6f24a45dd6454b
Guna <guna.klu@gmail.com>
2020-04-26 19:56:30 +0530

    using express

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const http = require("http");
-const { requestHandler } = require("./routes");
+const express = require("express");
 
-const server = http.createServer(requestHandler);
+const app = express();
 
-server.listen(3001);
+// app.use((req, res, next) => {
+//    console.log("First middleware");
+//    next();
+// });
+
+// app.use((req, res, next) => {
+//    console.log("Second middleware");
+//    res.send("<h1>Express works!</h1>")
+// });
+
+app.use("/users", (req, res, next) => {
+   res.send("<h1>Users</h1><ul><li>Guna</li><li>Sekar</li></ul>")
+});
+
+app.use("/", (req, res, next) => {
+   res.send("/ Nothing page!>")
+});
+
+
+app.listen(3001);
